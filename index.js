c8b727ed1fd5cc9299a8894cdfff8e34e75577a6
Muhammad Haider Sheikh <Haidersheikh243@gmail.com>
2024-03-24 22:47:30 +0500

    commiting

--- index.js
+++ index.js
@@ -24,6 +24,32 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/", (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+app.get("/api/timestamp/:date", (req, res) => {
+  let date = req.params.date;
+  if (/\d{5,}/.test(date)) {
+    date = parseInt(date);
+  }
+  date = new Date(date);
+  if (date.toString() === "Invalid Date") {
+    res.json({
+      error: "Invalid Date"
+    });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
+}
+);
 
 
 // Listen on port set in environment variable or default to 3000
